4528d2588dc58d5027210fa5defc51340d5e3bbe
AryamanDubey28 <89002729+AryamanDubey28@users.noreply.github.com>
2022-07-14 15:41:56 +0100

    Changed selection tile appearance

--- lib/central screens/nav bar routes/explore_page.dart
+++ lib/central screens/nav bar routes/explore_page.dart
@@ -13,19 +13,12 @@ class ExplorePage extends StatelessWidget {
           automaticallyImplyLeading: false,
           elevation: 0.0,
           backgroundColor: Colors.blue[200]),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 2.0,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [],
           ),
-          Center(
-            child: Lottie.network(
-                "https://assets6.lottiefiles.com/packages/lf20_tk3k6c0j.json"),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-        ],
+        ),
       ),
     );
   }
