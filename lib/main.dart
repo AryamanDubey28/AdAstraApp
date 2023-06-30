84b417b6b08a48124a7ecef43a171d2d444c1136
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2023-06-30 16:02:47 +0100

    build files

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,6 @@ Future main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
